c79e474bb763f0669774be80aee42d3344e5b8b3
unknown <hannahuseva@gmail.com>
2019-06-30 10:30:19 -0500

    Fixed out of order inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,61 +18,13 @@ function newOrder(){
         if (err) throw err
         console.log("connected as id " + connection.threadId+ "\n")
         showstock()
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "id",
-                message: "Please type the id number of the product you wish to buy\n",
-                validate: function(value) {
-                    if(value<11&&value>0){
-                        return true
-                    }
-                    else {
-                        return "Please type the id number!"
-                    }
-                }
-            },
-            {
-                type: "input",
-                name: "quantity",
-                message: "How many items would you like to order",
-                validate: function(value) {
-                    if(!isNaN(value)){
-                        return true
-                    }
-                    else {
-                        return "Please type number!"
-                    }  
-                }
-            }
-        ]).then(function (answers) {
-            chosenProduct = answers.id
-            connection.query("SELECT price, stock_quantity FROM products WHERE ?",
-            [
-                {
-                    item_id: answers.id
-                }
-            ],
-            function(err, res) {
-                if (err) throw err
-                if(res[0].stock_quantity >= parseFloat(answers.quantity)){
-                    new_quantity = res[0].stock_quantity-parseFloat(answers.quantity)
-                    total = parseFloat(answers.quantity)*res[0].price
-                    completeSale()
-                }
-                else{
-                    console.log("Insufficient quantity!\n")
-                    console.log("Sorry for inconvenience. More coming soon!")
-                    connection.end()
-                }
-            })
-        })
-    })
+    }) 
 }
 
 function showstock() {
     connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
         if (err) throw err
+        console.log("Today's best offers:\n")
         for (var i=0; i<res.length; i++){
             var stock={
                 ID: res[i].item_id,
@@ -81,6 +33,59 @@ function showstock() {
             }
             console.log(stock)  
         }
+        orderForm()
+    }) 
+}
+
+function orderForm(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "Please type the id number of the product you wish to buy\n",
+            validate: function(value) {
+                if(value<11&&value>0){
+                    return true
+                }
+                else {
+                    return "Please type the id number!"
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many items would you like to order",
+            validate: function(value) {
+                if(!isNaN(value)){
+                    return true
+                }
+                else {
+                    return "Please type number!"
+                }  
+            }
+        }
+    ]).then(function (answers) {
+        chosenProduct = answers.id
+        connection.query("SELECT price, stock_quantity FROM products WHERE ?",
+        [
+            {
+                item_id: answers.id
+            }
+        ],
+        function(err, res) {
+            if (err) throw err
+            if(res[0].stock_quantity >= parseFloat(answers.quantity)){
+                new_quantity = res[0].stock_quantity-parseFloat(answers.quantity)
+                total = parseFloat(answers.quantity)*res[0].price
+                completeSale()
+            }
+            else{
+                console.log("Insufficient quantity!\n")
+                console.log("Sorry for inconvenience. More coming soon!")
+                connection.end()
+            }
+        })
     })
 }
 
@@ -97,7 +102,7 @@ function completeSale(){
         ],
         function (err,res){
             if (err) throw err
-            console.log("Your total is: $"+ total)
+            console.log("Thank you for your order! Your total is: $"+ total)
             connection.end()
         }
     )
